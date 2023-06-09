d4efbd5d7a91c41b5ad3dcc28ece167f023ff56b
Artem Novikov <71457666+suleymaniac@users.noreply.github.com>
2023-06-09 21:38:46 +0300

    Add files via upload

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+static bool MatrixCheck(int[,] matrix, int n)
+{
+    if (n > 0 && n < 3)
+        return true;
+
+    int numberOfNegativeElementsInRow = 0;
+    int numberOfCorrectRows = 0;
+
+    for (int row = 0; row < n; row++)
+    {
+        for (int column = 0; column < n; column++)
+        {
+            if (matrix[row, column] < 0)
+                numberOfNegativeElementsInRow++;
+        }
+
+        if (numberOfNegativeElementsInRow <= 2)
+        {
+            numberOfCorrectRows++;
+            break;
+        }
+        numberOfNegativeElementsInRow = 0;
+    }
+
+    return numberOfCorrectRows > 0;
+}
+
+
+int[,] matrix = { 
+    { -1, -2, -3}, 
+    { 4, -5, -6}, 
+    { 7, -8, -9},
+};
+
+Console.WriteLine(MatrixCheck(matrix, 3));
